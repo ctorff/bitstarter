85549828df42f9fbd3909efd000748e9b8f359d4
Christian Torff <devel@torff.de>
2013-07-19 15:03:48 +0200

    added bootstrap and jquery

--- web.js
+++ web.js
@@ -7,12 +7,20 @@ app.get('/hello', function(request, response) {
   response.send('Hello World 2!');
 });
 
-app.get('/', function(request, response) {
-  var buff = fs.readFileSync('index.html');
+app.get('/', function(req, response) {
+  var buff = fs.readFileSync(__dirname + '/index.html');
   response.send(buff.toString());
 });
 
-var port = process.env.PORT || 5000;
+/*
+app.get('/:dir/:file', function(req, response) {
+  var buff = fs.readFileSync('static/' + req.params.dir + '/' + req.params.file);
+  response.send(buff.toString());
+});
+*/
+app.use(express.static(__dirname + '/static'));
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
